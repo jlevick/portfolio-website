b49243b8593c132335d1ce9392bf827f6177113d
Jaime Levick-Oxner <jaimelevick@gmail.com>
2022-12-01 17:05:18 -0700

    Added javascript
    Added JS file and menu toggle function. Issue with menu not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Oh hey there! Welcome to my portfolio site!");
+function menuToggle() {
+    var x = document.getElementById ('myNavtoggle') ;
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
